36136d440dbbd254a064946c6b67f97fc7c8e936
matiitelloo <matitellosojos10@gmail.com>
2026-07-27 21:40:45 -0500

    Creacion del ejercicio usando el ampersand

--- Clase7/ejercicio4.cpp
+++ Clase7/ejercicio4.cpp
@@ -5,3 +5,25 @@ Autor: Matías Tello S.
 Fecha: 27 de Julio de 2026
 
 */
+
+//PRIMER EJEMPLO SIN &
+
+#include <iostream>
+using namespace std;
+
+
+//FUNCION QUE SIRVE PARA AUMENTAR EL VALOR DE X
+void aumentar(int &x){
+    x = x + 1;
+    cout << "Dentro de la funcion, x vale: " << x << endl;
+
+}
+
+int main(){
+    int a = 10;
+    aumentar(a);
+
+    cout << "Fuera de la funcion, a vale: " << a << endl;
+
+    return 0;
+}
