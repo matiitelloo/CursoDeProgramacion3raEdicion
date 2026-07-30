4c3525de131bb2e031909c9a1d36a1f9c50e781d
matiitelloo <matitellosojos10@gmail.com>
2026-07-29 21:12:01 -0500

    Registro de datos

--- Clase8/Tello_Matias_Examen1.cpp
+++ Clase8/Tello_Matias_Examen1.cpp
@@ -39,7 +39,61 @@ do{
     cout << "Opcion invalida. Intente de nuevo: ";
     cin >> opcion;
     }
-    
+
+    switch(opcion) {
+            case 1:
+                cantidad = registrarEstudiantes(nombres, notas);
+                hayDatos = true; // Confirmamos que ya podemos usar las opciones 2 y 3
+                break;
+            case 2:
+                if (hayDatos == false) {
+                    cout << "\nError: Todavia no existen datos registrados.\n";
+                } else {
+                    mostrarReporte(nombres, notas, cantidad);
+                }
+                break;
+            case 3:
+                if (hayDatos == false) {
+                    cout << "\nError: Todavia no existen datos registrados.\n";
+                } else {
+                    buscarEstudiante(nombres, notas, cantidad);
+                }
+                break;
+            case 4:
+                cout << "\nSaliendo del sistema...\n";
+                break;
+        }
 } while (opcion != 4);
 
+return 0;
+
 }
+
+int registrarEstudiantes(string nombres[], float notas[]) {
+    int n;
+    cout << "Ingrese la cantidad de estudiantes para registrar (1-20): ";
+    cin >> n;
+
+    while (n < 1 || n > 20) {
+        cout << "Cantidad invalida. Ingrese un valor entre 1 y 20: ";
+        cin >> n;
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << "Estudiante " << i + 1 << ":" << endl;
+        cout << "Primer nombre: ";
+        cin >> nombres[i];
+
+        cout << "Nota final (0-20): ";
+        cin >> notas[i];
+
+        while (notas[i] < 0 || notas[i] > 20) {
+            cout << "Nota invalida. Vuelva a ingresar la nota (0-20): ";
+            cin >> notas[i];
+        }
+    }
+    
+    cout << "Datos registrados exitosamente.";
+    return n;
+
+    
