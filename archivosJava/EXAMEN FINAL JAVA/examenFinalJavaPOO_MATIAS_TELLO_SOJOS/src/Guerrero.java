dd3b68c34ab9404db92c0dc39b4b75feb8640950
matiitelloo <matitellosojos10@gmail.com>
2026-08-07 20:37:51 -0500

    Clase Guerrero.java terminada

--- archivosJava/EXAMEN FINAL JAVA/examenFinalJavaPOO_MATIAS_TELLO_SOJOS/src/Guerrero.java
+++ archivosJava/EXAMEN FINAL JAVA/examenFinalJavaPOO_MATIAS_TELLO_SOJOS/src/Guerrero.java
@@ -1,3 +1,12 @@
-public class Guerrero {
+public class Guerrero extends Personaje {
+    public Guerrero(int id; String nombre; int nivel) {
+        super(id, nombre, nivel)
+    }
+
+    @Override
+    public String realizarAccion(){
+
+        return getNombre() + " ataca con su espada";
+    }
 
 }
