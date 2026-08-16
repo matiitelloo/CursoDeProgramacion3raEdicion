a499dd5aed701c45c63b08784cc04c527194d0f2
matiitelloo <matitellosojos10@gmail.com>
2026-08-16 16:57:39 -0500

    Clase Telefono acabada

---
+++ archivosJava/Clase13/ejercicio2/src/Telefono.java
@@ -0,0 +1,47 @@
+public class Telefono {
+
+    private String marca;
+    private String modelo;
+    private double precio;
+
+    public Telefono(String marca, String modelo, double precio){
+        this.marca = marca;
+        this.modelo = modelo;
+        setPrecio(precio);
+    }
+    
+    public String getMarca(){
+        return marca;
+    }
+
+    public String getModelo(){
+        return modelo;
+    }
+
+    public double getPrecio(){
+        return precio;
+    }
+
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+    
+    public void setPrecio(double precio){
+        if (precio < 0) {
+            System.out.println("ERROR. El precio no puede ser negativo. Valor rechazado: " + precio);
+            return;
+        }
+        this.precio = precio;
+    }
+
+    public void mostrarInfo(){
+        System.out.println("Marca  : " + marca);
+        System.out.println("Modelo  : " + modelo);
+        System.out.println("Precio  : $" + String.format("%.2f", precio));
+        System.out.println("====================================");
+    }
+}
